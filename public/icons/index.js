515e5c536c6257d862db2cb07fb278291e9d1b8d
JonasStempickij <86933321+JonasStempickij@users.noreply.github.com>
2022-08-23 11:42:22 +0300

    git config core.ignorecase false

---
+++ public/icons/index.js
@@ -0,0 +1,25 @@
+import jsLogo from './javascript_logo.svg';
+import reactLogo from './react_logo.svg';
+import nextjsLogo from './nextjs_logo.svg';
+import tailwindcssLogo from './tailwindcss_logo.svg';
+import sassLogo from './sass_logo.svg';
+import boostrapLogo from './bootstrap_logo.svg';
+import reduxLogo from './redux_logo.svg';
+import gitLogo from './git_logo.svg';
+import figmaLogo from './figma_logo.svg';
+import mongoDBLogo from './mongodb_logo.svg';
+import firebaseLogo from './firebase_logo.svg';
+
+export const logoList = [
+    { name: 'Javascript', logo: jsLogo },
+    { name: 'React', logo: reactLogo },
+    { name: 'Next.js', logo: nextjsLogo },
+    { name: 'Tailwind', logo: tailwindcssLogo },
+    { name: 'Sass', logo: sassLogo },
+    { name: 'Bootstrap', logo: boostrapLogo },
+    { name: 'Redux', logo: reduxLogo },
+    { name: 'Git', logo: gitLogo },
+    { name: 'Figma', logo: figmaLogo },
+    { name: 'MongoDB', logo: mongoDBLogo },
+    { name: 'Firebase', logo: firebaseLogo },
+];
